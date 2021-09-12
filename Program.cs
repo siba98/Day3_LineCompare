2da995303ae49b4e1f14e018e91edec3bda0e14f
siba98 <gyansiba.patro@gmail.com>
2021-09-12 21:45:01 +0530

    Siba: added class compilation file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day3_LineCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start.Welcome();
+            UC1_CalculatingLength.Line1Length();
+            UC2_CheckEqualLength.CheckEqualLength();
+            UC3_Comparing2Lengths.CompareTwoLengths();
+        }
+    }
+}
