b3aa43feeca3d3149dbebb1432fd26dea4c750c3
siba98 <gyansiba.patro@gmail.com>
2021-09-12 21:42:23 +0530

    Welcome to Line Comparison Computation program

---
+++ start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day3_LineCompare
+{
+    class start
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to line comparison problem");
+        }
+    }
+}
