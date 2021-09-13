d22aed1784e92b9a27e465a2d4a5a9187da684e1
siba98 <gyansiba.patro@gmail.com>
2021-09-13 15:26:48 +0530

    refactor

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Day3_LineCompare
         static void Main(string[] args)
         {
             start.Welcome();
+            Console.WriteLine("Welcome to line comparison computation program");
             UC1_CalculatingLength.Line1Length();
             UC2_CheckEqualLength.CheckEqualLength();
             UC3_Comparing2Lengths.CompareTwoLengths();
