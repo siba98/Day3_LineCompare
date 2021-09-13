c4befce2f4197d48d6678cb5dea14bedef66fb6a
siba98 <gyansiba.patro@gmail.com>
2021-09-13 16:10:25 +0530

    Siba: added equal length checking between both line

--- UC2_CheckEqualLength.cs
+++ UC2_CheckEqualLength.cs
@@ -9,6 +9,7 @@ namespace Day3_LineCompare
         public static void CheckEqualLength()
         {
             int x1, x2, y1, y2;
+            Console.WriteLine("Enter coordinates for First line");
             Console.WriteLine("Enter values of the coordinates for first point(x1,y1)");
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
@@ -19,9 +20,9 @@ namespace Day3_LineCompare
             float line1length;
             line1length = (float)(Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2))));
             Console.WriteLine("Length of the line1 is: " + line1length);
-            string lengthOfLine1 = Convert.ToString(line1length);
-
+            
             int a1, a2, b1, b2;
+            Console.WriteLine("Enter coordintes for Second Line");
             Console.WriteLine("Enter values of the coordinates for first point(a1,b1)");
             a1 = Convert.ToInt32(Console.ReadLine());
             b1 = Convert.ToInt32(Console.ReadLine());
@@ -32,15 +33,14 @@ namespace Day3_LineCompare
             float line2length;
             line2length = (float)(Math.Sqrt((Math.Pow(a2 - a1, 2)) + (Math.Pow(b2 - b1, 2))));
             Console.WriteLine("Length of the line2 is: " + line2length);
-            string lengthOfLine2 = Convert.ToString(line2length);
-
-            if(lengthOfLine1 .Equals (lengthOfLine2))
+            
+            if(line1length .Equals (line2length))
             {
                 Console.WriteLine("Value of both the lines are equal");
             }
             else
             {
-                Console.WriteLine("Value of both the values are not equal");
+                Console.WriteLine("Value of both the lines are not equal");
             }
         }
     }
